268b4415e2e1a4e42a5f1989c7c013cb84a9b01e
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-10 11:25:39 +0100

    added fizzbuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,7 +7,12 @@ internal class Program
         for (int i = 0; i <= 100; i++)
         {
 
-            if (i % 3 == 0) 
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            
+            else if (i % 3 == 0) 
             {
                 Console.WriteLine("fizz");
             }
