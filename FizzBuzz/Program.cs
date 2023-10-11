548863ba8651980f413fd719046043c340cc445c
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-11 14:38:55 +0100

    added rules fro 13 and 17 and maximum number choice

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,38 +6,80 @@ internal class Program
     {
         return numerator % denomoinator == 0;
     }
+
+    private static int IsFoundLetter(List<string> message, string letter)
+    {
+        int result = -1;
+        for (int i = 0; i < message.Count(); i++)
+        {
+            if (message[i][0].ToString() == "B")
+            {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    
     static void Fizzbuzz()
     {
-        for (int i = 0; i <= 100; i++)
+        Console.WriteLine("Please enter a maximum number: ");
+        int maxNumChoice = Convert.ToInt32(Console.ReadLine());
+
+        List<string> result = new List<string>();
+        
+        for (int i = 1; i <= maxNumChoice; i++)
         {
-            string result = "";
+            result.Clear();
 
             if (IsDivisibleBy(i,3))
             {
-                result += "Fizz";
+                result.Add("Fizz");
             }
 
             if (IsDivisibleBy(i,5))
             {
-                result += "Buzz";
+                result.Add("Buzz");
             }
 
             if (IsDivisibleBy(i,7))
             {
-                result += "Bang";
+                result.Add("Bang");
             }
 
             if (IsDivisibleBy(i,11))
             {
-                result += "Bong";
-            }
-            
-            if (result == "")
-            {
-                result = i.ToString();
+                result.Add("Bong");
             }
 
-            Console.WriteLine(result);
+             if (IsDivisibleBy(i,13))
+            {
+                  if (result.Count > 0)
+                       {
+                           int position = IsFoundLetter(result, "B");
+                           if (position == -1) result.Add("Fezz");
+                           else result.Insert(position, "Fezz");
+                       }
+                       else
+                       {
+                           result.Clear();
+                           result.Add("Fezz");
+                       }
+
+            }
+
+            if (IsDivisibleBy(i,17))
+            {
+                result.Reverse();                          
+            }
+            
+            if (result.Count == 0)
+            {
+                result.Add(i.ToString());
+            }
+
+            Console.WriteLine(string.Join("", result));
            
         }
         Console.ReadLine();
